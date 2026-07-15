d6cb03b8565c92fc758ac82d2f9e948c16286355
kikson9 <tochukwuokike7@gmail.com>
2026-07-16 04:04:59 +0800

    fix: make password field optional in UserSerializer for editing users

--- api/serializers.py
+++ api/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username','first_name', 'last_name', 'student_id', 'email', 'role', 'is_active', 'status', 'password']
         extra_kwargs = {
-            'password': {'write_only':True}
+            'password': {'write_only': True, 'required': False}
         }
     
     def get_status(self, obj):
